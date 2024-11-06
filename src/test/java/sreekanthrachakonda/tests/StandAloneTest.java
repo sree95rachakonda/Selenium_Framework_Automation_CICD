9a09c5d566d935fe184ad6854f867291b62d0a11
sree95rachakonda <sree95rachakonda@gmail.com>
2024-11-06 01:54:11 -0500

    Updated StandAloneTest file

--- src/test/java/sreekanthrachakonda/tests/StandAloneTest.java
+++ src/test/java/sreekanthrachakonda/tests/StandAloneTest.java
@@ -20,7 +20,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-
+                // Testing CICD 
 		String productName = "ZARA COAT 3";
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
